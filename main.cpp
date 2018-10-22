7dee917a56ec0d966398402cb45eb5e940649569
Ari Sundholm <megari@iki.fi>
2018-10-22 19:26:02 +0300

    firmware: Implement basic operation with mode autodetection (composite only for now)

--- main.cpp
+++ main.cpp
@@ -26,8 +26,21 @@ public:
     }
 };
 
+template<typename RESET>
+class ADV7391 {
+public:
+    RESET reset;
+
+    unsigned char address;
+
+    ADV7391(unsigned char addr) : address(addr) {
+        reset.mode = OUTPUT;
+        reset = false;
+    }
+};
+
 ADV7280A<PortD2, PortD6, PortC2> decoder(0x20);
-// TODO: encoder
+ADV7391<PortD7> encoder(0x2a);
 
 PortC4 sda;
 PortC5 scl;
@@ -56,18 +69,19 @@ int main(void)
     _delay_ms(6);
     decoder.reset = true;
     decoder.pwrdwn = true;
+    encoder.reset = true;
     _delay_ms(6);
 
-#if 1
-while(1) {
 #if DEBUG
     serial << _T("converter starting...\r\n");
 #endif
 
-#if 1
+
+    // Decoder setup
+#if DEBUG
     serial << _T("writing I2C multi\r\n");
+#endif
     // Use CVBS input on A_in1
-    // INSEL[4:0] = 00000
     uint8_t insel[] = { 0x00, 0x00 };
     I2c_HW.write_multi(decoder.address, insel, insel + sizeof(insel));
 #if DEBUG
@@ -93,6 +107,15 @@ while(1) {
     else
         serial << _T("insel FAIL: 0x") << ashex(insel_rb) << _T("\r\n");
 #endif
+
+    // Autodetect SD video mode
+    uint8_t autodetect[] = { 0x02, 0x04 };
+    I2c_HW.write_multi(decoder.address, autodetect, autodetect + 2);
+
+    // Output control
+    // Enable output drivers
+    uint8_t outc[] = { 0x03, 0x0c };
+    I2c_HW.write_multi(decoder.address, outc, outc + sizeof(outc));
 
     // Extended output control
     // Output extended range, enable SFL, blank chroma during VBI etc.
@@ -120,6 +143,15 @@ while(1) {
         serial << _T("pwr mgmt FAIL: 0x") << ashex(rb) << _T("\r\n");
 #endif
 
+    // Analog clamp control
+    // 100% color bars
+    uint8_t ana_clampc[] = { 0x14, 0x11 };
+    I2c_HW.write_multi(decoder.address, ana_clampc, ana_clampc + 2);
+
+    // Analog Devices control 2
+    uint8_t adc2[] = { 0x1d, 0x40 };
+    I2c_HW.write_multi(decoder.address, adc2, adc2 + 2);
+
     // VS/FIELD Control 1
     // EAV/SAV codes generated for Analog Devices encoder
     uint8_t vs_fieldc[] = { 0x31, 0x02 };
@@ -132,9 +164,130 @@ while(1) {
     else
         serial << _T("VS/Field Control 1 FAIL: 0x") << ashex(rb) << _T("\r\n");
 #endif
+
+    // Output sync select 2
+    // Output SFL on the VS/FIELD/SFL pin
+    uint8_t out_sync_sel2[] = { 0x6b, 0x14 };
+    I2c_HW.write_multi(decoder.address, out_sync_sel2, out_sync_sel2 + 2);
+
+
+    // Encoder setup
+
+    // Software reset
+    uint8_t enc_reset_seq[] = { 0x17, 0x02 };
+    I2c_HW.write_multi(encoder.address, enc_reset_seq, enc_reset_seq + sizeof(enc_reset_seq));
+
+    // All DACs and PLL enabled
+    uint8_t seq_1_1[] = { 0x00, 0x1c };
+    I2c_HW.write_multi(encoder.address, seq_1_1, seq_1_1 + sizeof(seq_1_1));
+
+// Set this to 0 if you want to output the test pattern.
+#if 1
+    // SD input mode
+    uint8_t seq_1_2[] = { 0x01, 0x00 };
+    I2c_HW.write_multi(encoder.address, seq_1_2, seq_1_2 + sizeof(seq_1_2));
+
+    // NTSC, SSAF luma filter, 1.3MHz chroma filter
+    uint8_t seq_1_3[] = { 0x80, 0x10 };
+    I2c_HW.write_multi(encoder.address, seq_1_3, seq_1_3 + sizeof(seq_1_3));
 #endif
 
-    _delay_ms(5000);
-}
+    // Pixel data valid, YPrPb, SSAF PrPb filter, AVE control, pedestal
+    uint8_t seq_1_4[] = { 0x82, 0xc9 };
+    I2c_HW.write_multi(encoder.address, seq_1_4, seq_1_4 + sizeof(seq_1_4));
+
+    // Autodetect SD input standard
+    uint8_t sdm6[] = { 0x87, 0x20 };
+    I2c_HW.write_multi(encoder.address, sdm6, sdm6 + sizeof(sdm6));
+
+    // Enable SD progressive mode (for allowing 240p/288p)
+    uint8_t sdm7[] = { 0x88, 0x02 };
+    I2c_HW.write_multi(encoder.address, sdm7, sdm7 + sizeof(sdm7));
+
+// Set this to 1 if you want to output the test pattern.
+#if 0
+    // Test pattern
+    uint8_t pleasework1[] = { 0x84, 0x40 };
+    I2c_HW.write_multi(encoder.address, pleasework1, pleasework1 + 2);
 #endif
+
+
+#if DEBUG
+    // A simple loop to read and print out status changes in the decoder
+    uint8_t dec_status1 = 0x00;
+    uint8_t dec_status2 = 0x00;
+    uint8_t dec_status3 = 0x00;
+    while (1) {
+        I2c_HW.write(decoder.address, 0x10, true, false);
+        uint8_t new_status1 = I2c_HW.read(decoder.address);
+        I2c_HW.write(decoder.address, 0x12, true, false);
+        uint8_t new_status2 = I2c_HW.read(decoder.address);
+        I2c_HW.write(decoder.address, 0x13, true, false);
+        uint8_t new_status3 = I2c_HW.read(decoder.address);
+
+        if (new_status1 != dec_status1) {
+            serial << _T("Status 1 changed:\r\n");
+            serial << _T("In lock: ") << asdec(new_status1 & 0x01) << _T("\r\n");
+            serial << _T("Lost lock: ") << asdec(!!(new_status1 & 0x02)) << _T("\r\n");
+            serial << _T("fSC lock: ") << asdec(!!(new_status1 & 0x04)) << _T("\r\n");
+            serial << _T("Follow PW: ") << asdec(!!(new_status1 & 0x08)) << _T("\r\n");
+            serial << _T("Video standard: ");
+            switch ((new_status1 >> 4) & 0x07) {
+            case 0x00:
+                serial << _T("NTSC M/J\r\n");
+                break;
+            case 0x01:
+                serial << _T("NTSC 4.43\r\n");
+                break;
+            case 0x02:
+                serial << _T("PAL M\r\n");
+                break;
+            case 0x03:
+                serial << _T("PAL 60\r\n");
+                break;
+            case 0x04:
+                serial << _T("PAL B/G/H/I/D\r\n");
+                break;
+            case 0x05:
+                serial << _T("SECAM\r\n");
+                break;
+            case 0x06:
+                serial << _T("PAL Combination N\r\n");
+                break;
+            case 0x07:
+                serial << _T("SECAM 525\r\n");
+                break;
+            }
+            serial << _T("Color kill: ") << asdec(!!(new_status1 & 0x80)) << _T("\r\n");
+            serial << _T("\r\n");
+        }
+        dec_status1 = new_status1;
+
+        if (new_status2 != dec_status2) {
+            serial << _T("Status 2 changed:\r\n");
+            serial << _T("Line length nonstandard: ") << asdec(!!(new_status2 & 0x10)) << _T("\r\n");
+            serial << _T("fSC nonstandard: ") << asdec(!!(new_status2 & 0x20)) << _T("\r\n");
+            serial << _T("\r\n");
+        }
+        dec_status2 = new_status2;
+
+        if (new_status3 != dec_status3) {
+            serial << _T("Status 3 changed:\r\n");
+            serial << _T("Horizontal lock: ") << asdec(new_status3 & 0x01) << _T("\r\n");
+            serial << _T("Frequency: ") << ((new_status3 & 0x04) ? _T("50") : _T("60")) << _T("\r\n");
+            serial << _T("Freerun active: ") << asdec(!!(new_status3 & 0x10)) << _T("\r\n");
+            serial << _T("Field length standard: ") << asdec(!!(new_status3 & 0x20)) << _T("\r\n");
+            serial << _T("Interlaced: ") << asdec(!!(new_status3 & 0x40)) << _T("\r\n");
+            serial << _T("PAL SW lock: ") << asdec(!!(new_status3 & 0x80)) << _T("\r\n");
+            serial << _T("\r\n");
+        }
+        dec_status3 = new_status3;
+
+        _delay_ms(1000);
+    }
+#endif
+
+    I2c_HW.deinit();
+
+    return 0;
 }
