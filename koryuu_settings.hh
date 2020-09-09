7be5fe58560ff511b2fe547bdfc54f3248d69434
Ari Sundholm <megari@iki.fi>
2020-09-09 21:35:36 +0300

    firmware: Improve the EEPROM settings management code.

--- koryuu_settings.hh
+++ koryuu_settings.hh
@@ -10,15 +10,17 @@
 #include "crc32.hh"
 
 namespace koryuu_settings {
+    using yaal::autounion;
+
     constexpr char SETTINGS_MAGIC[8] =
         { 'K', 'R', 'Y', 'U', 'C', 'O', 'N', 'S' };
     constexpr uint16_t CURR_VERSION = 0x0001;
 
     enum Input : uint8_t {
-        CVBS,
-        CVBS_PEDESTAL,
-        SVIDEO,
-        SVIDEO_PEDESTAL,
+        CVBS = 0,
+        CVBS_PEDESTAL = 1,
+        SVIDEO = 2,
+        SVIDEO_PEDESTAL = 3,
     };
 
     enum PhysInput : uint8_t {
@@ -40,20 +42,27 @@ namespace koryuu_settings {
         [SVIDEO_PEDESTAL] = true,
     };
 
+    // The layout of this struct must remain compatible!
+    // This means that the any changes to the layout can
+    // only be done past the field min_read_version.
     struct SettingsHeader {
         char magic[8];
         uint32_t length;
         uint16_t version;
         uint16_t min_read_version;
         uint32_t checksum;
-    };
+    } __attribute__((packed));
+    static_assert(sizeof(SettingsHeader) == 20,
+        "SettingsHeader size is wrong!");
 
     struct ConvSettings {
         SettingsHeader hdr;
         Input default_input;
         uint8_t smoothing;
+        uint8_t padding[2];
         uint32_t checksum;
-    };
+    } __attribute__((packed));
+    static_assert(sizeof(ConvSettings) == 28, "ConvSettings size is wrong!");
 
     class KoryuuSettings {
     public:
@@ -86,27 +95,44 @@ namespace koryuu_settings {
                 if (hdr_checksum != tmp_hdr.checksum)
                     valid = false;
             }
+
             if (valid) {
-                uint32_t checksum, checksum_expected;
+                uint32_t checksum;
                 size_t checksum_ofs;
-                // TODO: check for ridiculous length
-                eeprom_read_block(&settings, eep_s, tmp_hdr.length);
-                if (tmp_hdr.length == sizeof(ConvSettings)) {
+                autounion<ConvSettings, false, 2 * sizeof(ConvSettings)> s_u;
+
+                // Check for ridiculous length
+                if (tmp_hdr.length > 2 * sizeof(ConvSettings))
+                    valid = false;
+                else
+                    eeprom_read_block(&s_u[0], eep_s, tmp_hdr.length);
+
+                if (valid && tmp_hdr.length == sizeof(ConvSettings)) {
+                    settings = s_u;
                     checksum_ofs = offsetof(decltype(settings), checksum);
                     checksum = settings.checksum;
                 }
-                else {
-                    const uintptr_t p_ =
-                        reinterpret_cast<uintptr_t>(&settings);
-                    const uint8_t *const p =
-                        reinterpret_cast<const uint8_t*>(p_);
+                else if (valid) {
+                    const uint8_t *const p = &s_u[0];
                     checksum_ofs = tmp_hdr.length - sizeof(uint32_t);
                     memcpy(&checksum, p + checksum_ofs, sizeof(uint32_t));
+
+                    // This intentionally "slices" a read-compatible
+                    // settings struct.
+                    settings = s_u;
                 }
-                checksum_expected =
-                    crc::crc32(&settings, checksum_ofs);
-                if (checksum != checksum_expected)
-                    valid = false;
+
+                if (valid) {
+                    uint32_t checksum_expected =
+                        crc::crc32(&settings, checksum_ofs);
+                    if (checksum != checksum_expected)
+                        valid = false;
+
+                    // Set the checksum of the in-memory struct
+                    // in case it had a different, while
+                    // read-compatible layout.
+                    settings.checksum = checksum_expected;
+                }
             }
 
             if (!valid) {
@@ -116,28 +142,34 @@ namespace koryuu_settings {
                 settings.hdr.length = sizeof(settings);
                 settings.hdr.version = CURR_VERSION;
                 settings.hdr.min_read_version = CURR_VERSION;
-                settings.hdr.checksum =
-                    crc::crc32(&settings.hdr,
-                        offsetof(decltype(settings.hdr), checksum));
                 settings.default_input = CVBS;
-                settings.smoothing = 0;
-                settings.checksum =
-                    crc::crc32(&settings,
-                        offsetof(decltype(settings), checksum));
+                settings.smoothing = 0x00;
+                memset(&settings.padding, 0x00, sizeof(settings.padding));
                 dirty = true;
             }
         }
 
+        YAAL_INLINE("KoryuuSettings::is_dirty()")
         bool is_dirty() const {
             return dirty;
         }
 
+        YAAL_INLINE("KoryuuSettings::set_dirty()")
         void set_dirty() {
-                dirty = true;
+            dirty = true;
         }
 
         void write() {
             if (dirty) {
+                settings.hdr.length = sizeof(settings);
+                settings.hdr.version = CURR_VERSION;
+                settings.hdr.min_read_version = CURR_VERSION;
+                settings.hdr.checksum =
+                    crc::crc32(&settings.hdr,
+                        offsetof(decltype(settings.hdr), checksum));
+                settings.checksum =
+                    crc::crc32(&settings,
+                        offsetof(decltype(settings), checksum));
                 eeprom_update_block(&settings,
                     eeprom_settings,
                     sizeof(settings));
