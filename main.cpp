16bdfa31da18bbeed10d7ba5296f844f29fa38f1
Ari Sundholm <megari@iki.fi>
2020-11-02 22:32:52 +0200

    firmware: Release test version 1.1-test1.

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@
 #include "debounce.hh"
 #include "koryuu_settings.hh"
 
-_T_DECL(FW_VERSION, "1.1-test0");
+_T_DECL(FW_VERSION, "1.1-test1");
 __attribute__((used))
 static const auto& FW_VERSION = _T_REF(FW_VERSION);
 
