30624af293882611edd82e3048f91628d9acdad8
Ari Sundholm <megari@iki.fi>
2020-12-06 21:46:17 +0200

    firmware: adv7391.hh: Add helper functions for setting SD and ED/HD mode registers.

--- adv7391.hh
+++ adv7391.hh
@@ -7,20 +7,41 @@
 #include <yaal/communication/i2c_hw.hh>
 
 namespace ad_encoder {
-	using namespace yaal;
+    using namespace yaal;
+    using namespace i2c_helpers;
 
-	template<typename RESET>
-	class ADV7391 {
-	public:
-	    RESET reset;
+    template<typename RESET>
+    class ADV7391 {
+    public:
+        RESET reset;
 
-	    unsigned char address;
+        unsigned char address;
 
-	    ADV7391(unsigned char addr) : address(addr) {
-		reset.mode = OUTPUT;
-		reset = false;
-	    }
-	};
+        ADV7391(unsigned char addr) : address(addr) {
+            reset.mode = OUTPUT;
+            reset = false;
+        }
+
+        void mode_select(uint8_t value) {
+            I2C_WRITE(address, 0x01, value);
+        }
+
+        void set_mode0(uint8_t value) {
+            I2C_WRITE(address, 0x02, value);
+        }
+
+        void set_ed_hd_mode1(uint8_t value) {
+            I2C_WRITE(address, 0x30, value);
+        }
+
+        void set_ed_hd_mode2(uint8_t value) {
+            I2C_WRITE(address, 0x31, value);
+        }
+
+        void set_ed_hd_mode3(uint8_t value) {
+            I2C_WRITE(address, 0x32, value);
+        }
+    };
 }
 
 #endif
